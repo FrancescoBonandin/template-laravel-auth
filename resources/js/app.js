341ebdc861adc5ae2dec83aad56f05d07bcff33c
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-09-20 09:01:14 +0200

    Edit  v1.0 template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 import './bootstrap';
 
-
 import '~resources/scss/app.scss';
+
+import * as bootstrap from 'bootstrap';
+
+import.meta.glob([
+    '../img/**'
+]);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import path from 'path';
+
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -16,7 +19,8 @@ export default defineConfig({
 
         alias:{
 
-            '~resources': '/resource/'
+            '~resources': '/resource/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
 
         }
 
